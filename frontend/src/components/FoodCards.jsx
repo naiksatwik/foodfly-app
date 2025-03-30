6bf6f3a4c7a8223814a3763b27feffa203311cc5
naiksatwik <codeguy181@gmail.com>
2025-03-30 17:23:51 +0530

    Converted frontend from submodule to normal folder

---
+++ frontend/src/components/FoodCards.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useCartContext } from "../context/CartContext";
+
+const FoodCards = (props) => {
+  const { addItem } = useCartContext();
+  const ut=localStorage.getItem('UserType');
+  const Price=Number(props.price);
+  return (
+    <div className="shadow-2xl rounded-lg hover:scale-110 duration-200 w-[17rem] sm:w-full">
+      <img
+        src={props.image}
+        alt={props.name}
+        className="  w-full  h-[8rem] object-cover  rounded-tl-lg rounded-tr-lg"
+      />
+      {
+       (ut == "User"|| ut =='user' || ut == null)?
+       <div className="flex py-2 px-3 justify-between">
+       <div className="w-1/2">
+         <p className="text-[12px] text-gray-500 font-medium">{props.name}</p>
+         <p className="text-sm font-medium pt-2">₹{props.price}</p>
+       </div>
+       <div className="w-1/3 flex items-center justify-center">
+         <button
+           className="orangeButton px-4 text-sm hover:bg-green-600  hover:text-white  hover:border-green-600"
+           onClick={() =>
+             addItem(
+               props.id,
+               Price,
+               props.image,
+               props.name,
+               props.noItem
+             )
+           }
+         >
+           Add{" "}
+         </button>
+       </div>
+     </div>:
+     <div className="flex py-2 px-3 justify-between">
+     <div className="w-1/2">
+       <p className="text-[12px] text-gray-500 font-medium">{props.name}</p>
+       <p className="text-sm font-medium pt-1">₹{props.price}</p>
+       <p className="text-sm font-medium pt-2"><span className="text-md text-gray-500 font-medium">Id </span>{props.id}</p>
+     </div>
+     <div className="w-1/3 flex items-center justify-center">
+       <button
+       id="but"
+         className="orangeButton px-4 text-sm hover:bg-green-600  hover:text-white  hover:border-green-600"
+         onClick={() =>
+           addItem(
+             props.id,
+             Price,
+             props.image,
+             props.name,
+             props.noItem
+           )
+         }
+       >
+         Add{" "}
+       </button>
+     </div>
+   </div>
+      }
+
+    </div>
+  );
+};
+
+export default FoodCards;
---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,98 @@
+import React, { useContext, useState } from "react";
+import Navbar from "../components/Navbar";
+import { MdAddShoppingCart } from "react-icons/md";
+import CartItem from "../components/CartItem";
+import { useCartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  const { cart, totalCost, totalItem } = useCartContext();
+  const[address,setAddress]=useState(null)
+  let userName = window.localStorage.getItem("userName");
+  let userEmail=localStorage.getItem('userEmail')
+  let userAdd;
+  let userPhone=localStorage.getItem('userPhone')
+  if(address == null){
+    userAdd=localStorage.getItem('userAddress')
+  }else{
+    userAdd=address;
+  }
+  const totalCosts = totalCost();
+  const submit=async()=>{
+    cart.push(totalCosts);
+    let response= await fetch("http://localhost:5000/api/ProductToAdmin",{
+    method: "POST",
+    crossDomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      email:userEmail,
+      order_data:cart,
+      address:userAdd,
+      userName,
+      phone:userPhone,
+      Time:new Date().toString().slice(0,25)
+    }),
+  }).then(res=>{
+    console.log("cart Page",res)
+    if(res.status === 200){
+  console.log(cart)
+
+    localStorage.removeItem('CartItem');
+
+    location.reload()
+  }
+  }) 
+  }
+  return (
+    <div>
+      <Navbar />
+      <div>
+        {cart.map((product) => {
+          return <CartItem data={product} />;
+        })}
+
+        {cart.length > 0 ? (
+          <div className="max-w-[600px] mx-auto flex flex-col items-end pr-8 ">
+            <input
+              type="text"
+              placeholder="Add address"
+              className="border-2 border-black p-1 rounded-full mt-10 pl-6"
+              onChange={(eve)=>{
+                setAddress(eve.target.value)
+
+              }}
+            />
+             
+            <h1 className=" text-2xl font-bold  px-10 pt-4 ">
+              Total: ₹{totalCosts}
+            </h1>
+            {userName == null ? (
+              <Link to="/sigin">
+                <button className="mt-4 bg-black text-white px-4 py-2 rounded-full mr-10" >
+                  Login for Order
+                </button>
+              </Link>
+            ) : (
+                <button className="mt-4 bg-black text-white px-4 py-2 rounded-full mr-10" onClick={()=>{
+                submit()
+                }  } type="submit">
+                  place Order
+                </button>
+            )}
+          </div>
+        ) : (
+          <div className="flex h-[80vh] w-full items-center justify-center space-x-4">
+            <h1 className="text-4xl  text-center ">Cart is Empty</h1>
+            <MdAddShoppingCart size={50} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
